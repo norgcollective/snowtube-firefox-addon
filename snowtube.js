368b36df71753caa1ec1ba5e7659e124c172400c
heschy2 <105609148+heschy2@users.noreply.github.com>
2022-11-01 22:21:23 +0100

    Update snowtube.js

--- snowtube.js
+++ snowtube.js
@@ -1,11 +1,28 @@
-setTimeout(function() {
+function snowify() {
+    var snowflake_center  = "<center id=\"snowtube-snowflake-container\" style=\"margin: 1cm;\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></center>"
+    var snowflake         = "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm;padding:5mm\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
+    var snowflake_sidebar = "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm;float;right;\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
     if(window.location.host == "www.youtube.com" && window.location.pathname =="/watch") {
-        document.getElementById('chips').parentElement.parentElement.outerHTML = "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
+        document.getElementById('chips').parentElement.parentElement.outerHTML = iframe_std
     }
     else if(window.location.host.split('.')[1] == "wikipedia" && window.location.pathname.split('/')[1] == "wiki" ) {
-        document.getElementById('catlinks').outerHTML += "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm;min-width: 320px;\"><iframe scrolling=\"yes\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;max-height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe><p style=\"font-size:7.75pt;font-family: arial,sans,monospace,serif;\">This Snowflake has been added to Wikipedia by the Snowtube Addon</p></div>"
+        document.getElementById('catlinks').outerHTML += snowflake_center
     }
-    else if(window.location.host == "www.instagram.com" && window.location.pathname.split('/')[1] =="direct") {
-        document.getElementsByClassName('_ab8w  _ab94 _ab97 _ab9h _ab9m _ab9p  _aba8 _abcm')[0].outerHTML = "<div id=\"snowtube-snowflake-container\" style=\"margin: .5cm\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
+    else if(window.location.host.split('.')[1] == "nytimes") {
+        document.getElementById('app').innerHTML += snowflake
     }
-}, 7500);
+    else if (window.location.host.split('.')[1] == "spiegel" && window.location.host.split('.')[2] == "de") { 
+        document.getElementsByClassName('lg:sticky')[0].outerHTML = iframe_std
+    }
+    else if (window.location.host.split('.')[1] == "zeit" && window.location.host.split('.')[2] == "de") { 
+        document.getElementsByClassName('page__content')[0].innerHTML += snowflake_center
+    }
+    else if (window.location.host.split('.')[1] == "amazon") { 
+        document.getElementById('a-page').innerHTML += snowflake_center
+    }
+}
+
+setTimeout(function() {if(document.getElementById('snowtube-snowflake-container') == null) {snowify()}}, 500);
+setTimeout(function() {if(document.getElementById('snowtube-snowflake-container') == null) {snowify()}}, 5000);
+setTimeout(function() {if(document.getElementById('snowtube-snowflake-container') == null) {snowify()}}, 20000);
+setTimeout(function() {if(document.getElementById('snowtube-snowflake-container') == null) {snowify()}}, 60000);
