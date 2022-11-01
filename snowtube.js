8555e19fa8236ebc7369da80966cf915aa71dfc1
heschy2 <105609148+heschy2@users.noreply.github.com>
2022-11-01 22:32:42 +0100

    Update snowtube.js

--- snowtube.js
+++ snowtube.js
@@ -3,7 +3,7 @@ function snowify() {
     var snowflake         = "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm;padding:5mm\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
     var snowflake_sidebar = "<div id=\"snowtube-snowflake-container\" style=\"margin: 1cm;float;right;\"><iframe scrolling=\"no\" src=\"https://snowflake.torproject.org/embed.html\" style=\"border-radius: 5mm;min-width: 320px;height: 285px;box-shadow: 0 0rem 2rem .125rem rgb(20, 0,20, 10)\" frameborder=\"0\"></iframe></div>"
     if(window.location.host == "www.youtube.com" && window.location.pathname =="/watch") {
-        document.getElementById('chips').parentElement.parentElement.outerHTML = iframe_std
+        document.getElementById('chips').parentElement.parentElement.outerHTML = snowflake
     }
     else if(window.location.host.split('.')[1] == "wikipedia" && window.location.pathname.split('/')[1] == "wiki" ) {
         document.getElementById('catlinks').outerHTML += snowflake_center
